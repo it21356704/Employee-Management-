b17a65f1c54bb8ca0a7882750ddd30963d94d60e
IT21356704 <it21356704@my.sliit.lk>
2024-01-03 00:40:06 +0530

    first commit

---
+++ src/main/java/com/example/HR_Management_App/HrManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.HR_Management_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagementAppApplication.class, args);
+	}
+
+}
